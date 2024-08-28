c53bb247760c5d652d091134dbf02895107853c1
Kyaw Thu <evanch98@gmail.com>
2024-08-28 17:53:56 +0800

    updating the SignInCard

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,3 +1,82 @@
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+
 export const SignInCard = () => {
-  return <div>Sign In Card</div>;
+  return (
+    <Card className="h-full w-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Log in to continue</CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Button
+            type="submit"
+            size="lg"
+            disabled={false}
+            className="w-full"
+          >
+            Continue
+          </Button>
+        </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            disabled={false}
+            className="relative w-full"
+          >
+            <FcGoogle className="absolute left-2.5 top-3 size-5" />
+            Continue with Google
+          </Button>
+          <Button
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            disabled={false}
+            className="relative w-full"
+          >
+            <FaGithub className="absolute left-2.5 top-3 size-5" />
+            Continue with GitHub
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Don&apos;t have an account?{' '}
+          <span className="cursor-pointer text-sky-700 hover:underline">
+            Sign up
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
 };
