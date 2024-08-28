679735d199cd7102416d26769818667b8ade982f
Kyaw Thu <evanch98@gmail.com>
2024-08-28 18:01:07 +0800

    updating the SignUpCard

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,3 +1,99 @@
-export const SignUpCard = () => {
-  return <div>Sign Up Card</div>;
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import { SignInFlow } from '../types';
+
+interface SignUpCardProps {
+  // eslint-disable-next-line no-unused-vars
+  setState: (state: SignInFlow) => void;
+}
+
+export const SignUpCard = ({ setState }: SignUpCardProps) => {
+  return (
+    <Card className="h-full w-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Sign up to continue</CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Confirm password"
+            type="password"
+            required
+          />
+          <Button
+            type="submit"
+            size="lg"
+            disabled={false}
+            className="w-full"
+          >
+            Continue
+          </Button>
+        </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            disabled={false}
+            className="relative w-full"
+          >
+            <FcGoogle className="absolute left-2.5 top-3 size-5" />
+            Continue with Google
+          </Button>
+          <Button
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            disabled={false}
+            className="relative w-full"
+          >
+            <FaGithub className="absolute left-2.5 top-3 size-5" />
+            Continue with GitHub
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Already have an account?{' '}
+          <span
+            onClick={() => setState('signIn')}
+            className="cursor-pointer text-sky-700 hover:underline"
+          >
+            Sign in
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
 };
