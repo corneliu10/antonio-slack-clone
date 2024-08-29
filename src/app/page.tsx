c53560838136dbc829bad194f3ccbe9a099bf6b5
Kyaw Thu <evanch98@gmail.com>
2024-08-29 19:14:50 +0800

    updating the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import { AuthScreen } from '@/features/auth/components/auth-screen';
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useAuthActions } from '@convex-dev/auth/react';
 
 const HomePage = () => {
-  return <AuthScreen />;
+  const { signOut } = useAuthActions();
+
+  return (
+    <div>
+      Logged In <Button onClick={() => signOut()}>Sign Out</Button>
+    </div>
+  );
 };
 
 export default HomePage;
