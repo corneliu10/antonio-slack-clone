4a8826b9c615babea1c61e486225a4b835fd1e87
Kyaw Thu <evanch98@gmail.com>
2024-08-31 19:39:15 +0800

    updating the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 import { UserButton } from '@/features/auth/components/user-button';
+import { useCreateWorkspaceModal } from '@/features/store/use-create-workspace-modal';
 import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces';
 import { useEffect, useMemo } from 'react';
 
 const HomePage = () => {
+  const [open, setOpen] = useCreateWorkspaceModal();
+
   const { data, isLoading } = useGetWorkspaces();
 
   const workspaceId = useMemo(() => data?.[0]?._id, [data]);
@@ -14,10 +17,10 @@ const HomePage = () => {
 
     if (workspaceId) {
       console.log('Redirect to workspace');
-    } else {
-      console.log('Open creation modal');
+    } else if (!open) {
+      setOpen(true);
     }
-  }, [isLoading, workspaceId]);
+  }, [isLoading, workspaceId, open, setOpen]);
 
   return (
     <div>
