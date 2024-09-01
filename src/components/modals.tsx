020a5b2bb7d7e608cbc6cd613c3d72a79b2c949f
Kyaw Thu <evanch98@gmail.com>
2024-09-01 13:11:54 +0800

    fixing the hydration errors

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -1,8 +1,19 @@
 'use client';
 
 import { CreateWorkspaceModal } from '@/features/workspaces/components/create-workspace-modal';
+import { useEffect, useState } from 'react';
 
 export const Modals = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <>
       <CreateWorkspaceModal />
