4bb5f645b2421ccbb69bd7780a8250ffbbb68996
Kyaw Thu <evanch98@gmail.com>
2024-09-01 16:48:48 +0800

    adding the Toaster provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ConvexClientProvider } from '@/components/convex-client-provider';
 import { Modals } from '@/components/modals';
+import { Toaster } from 'sonner';
 import { ConvexAuthNextjsServerProvider } from '@convex-dev/auth/nextjs/server';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -23,6 +24,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ConvexClientProvider>
+            <Toaster />
             <Modals />
             {children}
           </ConvexClientProvider>
